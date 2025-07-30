e13464f6ac69e2d743a95df5db0028f3562f4418
T T <hdang4747@gmail.com>
2025-07-31 00:49:13 +0700

    more fixed data

--- lib/models/playlist_provider.dart
+++ lib/models/playlist_provider.dart
@@ -7,43 +7,128 @@ import 'package:music_app/models/songs.dart';
 //this file for playlist
 class PlaylistProvider extends ChangeNotifier {
   final List<Song> _playList = [
-    //list of songs (idk how to make it automatically match the song and img name)
     //index 0
     Song(
       songName: 'Calm',
       artistName: 'Cursedsnake',
-      albumImage: 'assets/images/calm_img.png',
+      albumImage: 'assets/images/calm.png',
       audioPath: 'audio/Calm.mp3',
-      genre: 'Jazz', // Thể loại mới
+      genre: 'EDM', // Thể loại mới
     ),
     //index 1
     Song(
       songName: 'Feel',
       artistName: '8bitPice',
-      albumImage: 'assets/images/feel_img.png',
+      albumImage: 'assets/images/feel.png',
       audioPath: 'audio/Feel.mp3',
-      genre: 'EDM', // Thể loại mới
+      genre: 'EDM',
     ),
+    //2
     Song(
       songName: 'Unity',
       artistName: 'TheFatRat',
-      albumImage: 'assets/images/Unity_img.png',
+      albumImage: 'assets/images/unity.png',
       audioPath: 'audio/Unity.mp3',
-      genre: 'EDM', // Thể loại mới
+      genre: 'EDM',
     ),
+    //3
     Song(
       songName: 'Invisible',
       artistName: 'Duran Duran',
       albumImage: 'assets/images/invisible.png',
       audioPath: 'audio/Invisible.mp3',
-      genre: 'Pop', // Thể loại mới
+      genre: 'Pop',
     ),
+    //4
     Song(
-      songName: 'Tuyen ngon doc lap',
-      artistName: 'Ho Chi Minh',
-      albumImage: 'assets/images/image00.png',
-      audioPath: 'audio/Tndl.mp3',
-      genre: 'Speaking Audio', // Thể loại mới
+      songName: 'Blinding Lights',
+      artistName: 'The Weeknd',
+      albumImage: 'assets/images/blinding_lights.png',
+      audioPath: 'audio/BlindingLights.mp3',
+      genre: 'Synthwave',
+    ),
+    //5
+    Song(
+      songName: 'Bohemian Rhapsody',
+      artistName: 'Queen',
+      albumImage: 'assets/images/bohemian_rhapsody.png',
+      audioPath: 'audio/BohemianRhapsody.mp3',
+      genre: 'Rock',
+    ),
+    //6
+    Song(
+      songName: 'Stay',
+      artistName: 'The Kid LAROI & Justin Bieber',
+      albumImage: 'assets/images/stay.png',
+      audioPath: 'audio/Stay.mp3',
+      genre: 'Pop',
+    ),
+    //7
+    Song(
+      songName: 'Uptown Funk',
+      artistName: 'Mark Ronson ft. Bruno Mars',
+      albumImage: 'assets/images/uptown_funk.png',
+      audioPath: 'audio/UptownFunk.mp3',
+      genre: 'Funk/Pop',
+    ),
+    //8
+    Song(
+      songName: 'Cake by the ocean',
+      artistName: 'DNCE',
+      albumImage: 'assets/images/cake_by_the_ocean.png',
+      audioPath: 'audio/CakeByTheOcean.mp3',
+      genre: 'Pop',
+    ),
+
+    //9
+    Song(
+      songName: 'Blowin’ in the Wind',
+      artistName: 'Bob Dylan',
+      albumImage: 'assets/images/blowin_in_the_wind.png',
+      audioPath: 'audio/BlowinInTheWind.mp3',
+      genre: 'Folk',
+    ),
+
+    //10
+    Song(
+      songName: 'Can’t Stop the Feeling!',
+      artistName: 'Justin Timberlake',
+      albumImage: 'assets/images/cant_stop_the_feeling.png',
+      audioPath: 'audio/CantStopTheFeeling.mp3',
+      genre: 'Pop',
+    ),
+
+    //11
+    Song(
+      songName: 'Thunderstruck',
+      artistName: 'AC/DC',
+      albumImage: 'assets/images/thunderstruck.png',
+      audioPath: 'audio/Thunderstruck.mp3',
+      genre: 'Hard Rock',
+    ),
+    //12
+    Song(
+      songName: 'Counting Stars',
+      artistName: 'OneRepublic',
+      albumImage: 'assets/images/counting_stars.png',
+      audioPath: 'audio/CountingStars.mp3',
+      genre: 'Pop Rock',
+    ),
+    //13
+    Song(
+      songName: 'Lovely',
+      artistName: 'Billie Eilish & Khalid',
+      albumImage: 'assets/images/lovely.png',
+      audioPath: 'audio/Lovely.mp3',
+      genre: 'Indie Pop',
+    ),
+    //14
+    Song(
+      songName: 'Africa',
+      artistName: 'Toto',
+      albumImage: 'assets/images/africa.png',
+      audioPath: 'audio/Africa.mp3',
+      genre: 'Soft Rock',
     ),
   ];
   /*AUDIOPLAYER 
